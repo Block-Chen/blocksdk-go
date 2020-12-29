957db9b7d1ad32cb50100124e6584fc73577a576
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2020-12-29 20:18:46 +0900

    Add files via upload

--- ethereum/ethereum.go
+++ ethereum/ethereum.go
@@ -1,167 +1,154 @@
 package ethereum
 
-import (  
-    "fmt"
-    "time"
-    "strconv"
-    "net/http"
-    "encoding/json"
-    "log"
-    "strings"
-    "blocksdk-go/base"
+import ( 
+    "github.com/Block-Chen/blocksdk-go/base"
 )
 
 type Ethereum struct{
-    Base base.Base
+    Base *base.Base
 }
 
-func getBlockChain(request = map[string]interface{}) *Ethereum {
-	return Base.request("GET","/eth/block")		
+func NewEthereumClient(api_token string) *Ethereum {
+	b := base.NewBase(api_token)
+	return &Ethereum{
+		Base : b,
+	}
+}
+
+func (e *Ethereum) GetBlockChain() map[string]interface{}{
+	return e.Base.Request("GET","/eth/block",nil)		
 }
 
 
-func getBlock(request = map[string]interface{}) *Ethereum {
-    value, ok := request['rawtx']
+func (e *Ethereum) GetBlock(request map[string]interface{}) map[string]interface{} {
+    _, ok := request["rawtx"]
     if !ok {
-            request['rawtx'] = false
+            request["rawtx"] = false
     }
-    value2, ok2 := request['offset']
+    _, ok2 := request["offset"]
     if !ok2 {
-            request['offset'] = 0
+            request["offset"] = 0
     }
-
-    value3, ok3 := request['limit']
-
-    if !ok2 {
-            request['limit'] = 10
+    _, ok3 := request["limit"]
+    if !ok3 {
+            request["limit"] = 10
     }        
-	return Base.request("GET","/eth/block/" + strconv.Itoa(request['block']),{"rawtx" : request['rawtx'],"offset": request['offset'],"limit" : request['limit']})
-
-
+	return e.Base.Request("GET","/eth/block/" + request["block"].(string),request)
 }
 
 
-func getMemPool(request = map[string]interface{}) *Ethereum {
+func (e *Ethereum) GetMemPool(request map[string]interface{}) map[string]interface{} {
 
-        value, ok := request['rawtx']
+	_, ok := request["rawtx"]
     if !ok {
-            request['rawtx'] = false
+		request["rawtx"] = false
     }
-    value2, ok2 := request['offset']
+    _, ok2 := request["offset"]
     if !ok2 {
-            request['offset'] = 0
+		request["offset"] = 0
     }
-
-    value3, ok3 := request['limit']
-
-    if !ok2 {
-            request['limit'] = 10
-    }  
-	return self.request("GET","/eth/mempool",{"rawtx" : request['rawtx'],"offset" : request['offset'],"limit" : request['limit']})
-
+    _, ok3 := request["limit"]
+    if !ok3 {
+		request["limit"] = 10
+    } 
+	
+	return e.Base.Request("GET","/eth/mempool",request)
 }
 
 
-func listAddress(request = map[string]interface{}) *Ethereum {
-    value, ok := request['offset']
+func (e *Ethereum)ListAddress(request map[string]interface{}) map[string]interface{}{
+    _, ok := request["offset"]
     if !ok {
-            request['offset'] = 0
+		request["offset"] = 0
     }
-    value2, ok2 := request['limit']
+    _, ok2 := request["limit"]
     if !ok2 {
-            request['limit'] = 10
+		request["limit"] = 10
     }
 
 		
-	return Base.request("GET","/eth/address",{
-			"offset" : request['offset'],
-			"limit" : request['limit']
-		})   
+	return e.Base.Request("GET","/eth/address",request)   
 }
 
 
 
-func loadAddress(request = map[string]interface{}) *Ethereum {
-	return Base.request("POST","/eth/address/" + request['address'] + "/load",{"seed_wif" : request['seed_wif'],"password" : request['password']})
+func (e *Ethereum)LoadAddress(request map[string]interface{})  map[string]interface{} {
+	return e.Base.Request("POST","/eth/address/" + request["address"].(string) + "/load",request)
 
 }
 
 
-func unLoadAddress(request = map[string]interface{}) *Ethereum {
-	return Base.request("POST","/eth/address/" + request['address'] + "/unload")
+func (e *Ethereum)UnLoadAddress(request  map[string]interface{})  map[string]interface{} {
+	return e.Base.Request("POST","/eth/address/" + request["address"].(string) + "/unload",nil)
 }
 
 
-func createAddress(request = map[string]interface{}) *Ethereum {
-   valuee, okk := request['name']
+func (e *Ethereum)CreateAddress(request  map[string]interface{})  map[string]interface{} {
+	_, okk := request["name"]
     if !okk {
-            request['name'] = nil
+		request["name"] = nil
     }	
-	return Base.request("POST","/eth/address",{"name" : request['name']})
+	return e.Base.Request("POST","/eth/address",request)
 }
 
 
-func getAddressInfo(request = map[string]interface{}) *Ethereum {
-   valuee, okk := request['reverse']
+func (e *Ethereum)getAddressInfo(request  map[string]interface{})  map[string]interface{} {
+	_, okk := request["reverse"]
     if !okk {
-            request['reverse'] = true
+		request["reverse"] = true
     }
-    value, ok := request['rawtx']
+    _, ok := request["rawtx"]
     if !ok {
-            request['rawtx'] = false
+		request["rawtx"] = false
     }
 
-    value2, ok2 := request['offset']
+    _, ok2 := request["offset"]
     if !ok2 {
-            request['offset'] = 0
+		request["offset"] = 0
     }
 
-    value3, ok3 := request['limit']
-
-    if !ok2 {
-            request['limit'] = 10
+    _, ok3 := request["limit"]
+    if !ok3 {
+		request["limit"] = 10
     }  
 	
-	return Base.request("GET","/eth/address/" + request['address'],{"reverse" : request['reverse'],"rawtx"  : request['rawtx'],"offset" : request['offset'],"limit"  : request['limit']})
+	return e.Base.Request("GET","/eth/address/" + request["address"].(string) ,request)
 
     
 }
 
-func getAddressBalance(request = map[string]interface{}) *Ethereum {
+func (e *Ethereum)GetAddressBalance(request  map[string]interface{})  map[string]interface{} {
 
-	return Base.request("GET","/eth/address/" + request['address'] + "/balance")
+	return e.Base.Request("GET","/eth/address/" + request["address"].(string) + "/balance",nil)
 
 }
-func sendToAddress(request = map[string]interface{}) *Ethereum {
+func (e *Ethereum)SendToAddress(request  map[string]interface{})  map[string]interface{} {
 
-        value, ok := request['kbfee']
+	_, ok := request["kbfee"]
     if !ok {
-
-        blockChain = *getBlockChain()
-        request['kbfee'] = blockChain['medium_fee_per_kb']
+        blockChain := e.GetBlockChain()
+        request["kbfee"] = blockChain["medium_fee_per_kb"].(float64)
     }
 
-    value1, ok1 := request['seed_wif']
+    _, ok1 := request["seed_wif"]
     if !ok1 {
-            request['seed_wif'] = nil
+		request["seed_wif"] = nil
     }
-    value2, ok2 := request['password']
+    _, ok2 := request["password"]
     if !ok2 {
-            request['password'] = nil
+		request["password"] = nil
     }
 
-	return Base.request("POST","/eth/address/" + request['from'] + "/sendtoaddress",{"address" : request['to'],"amount" : request['amount'],"private_key" : request['private_key'],"password" : request['password'],"gwei" : request['gwei']})
+	return e.Base.Request("POST","/eth/address/" + request["from"].(string) + "/sendtoaddress",request)
 
 }
 
-func sendTransaction(request = map[string]interface{}) *Ethereum {
-	return Base.request("POST","/eth/transaction",{"sign_hex" : request['sign_hex']})
+func (e *Ethereum)SendTransaction(request map[string]interface{})  map[string]interface{} {
+	return e.Base.Request("POST","/eth/transaction",request)
 
 }
-func getTransaction(request = map[string]interface{}) *Ethereum {
-	return Base.request("GET","/eth/transaction/" + request['hash'])
-
-    
+func (e *Ethereum)GetTransaction(request map[string]interface{})  map[string]interface{} {
+	return e.Base.Request("GET","/eth/transaction/" + request["hash"].(string),nil)
 }
 
 
